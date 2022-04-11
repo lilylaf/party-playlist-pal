ca9a5414b7e1b81c54df654ec0c3b8702c8903ff
Scott Kloustin <skloustin@gmail.com>
2022-04-11 15:00:18 -0400

    vue get events request working

---
+++ vue/src/components/EventSearch.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+      <h2>This is the title of the Event Search</h2>
+      <div v-for="event in events" v-bind:key="event.id">
+         <hr>
+          <h2>{{ event.eventName }}</h2>
+          <p>{{ event.information }}</p>
+      </div>
+  </div>
+</template>
+
+<script>
+import eventService from '../services/EventService.js'
+
+
+export default {
+    name: 'EventSearch',
+    data(){
+        return {
+            events:[]
+        }
+    },
+
+    created(){
+        eventService.getEvents()
+        .then((response) => {
+            this.events = response.data;
+            console.log(response.data);
+        })
+    }
+
+
+}
+</script>
+
+<style scoped>
+    
+</style>
---
+++ vue/src/services/EventService.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export default {
+    getEvents() {
+        return axios.get('/events')
+    }
+
+
+}
