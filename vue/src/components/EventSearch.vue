b633057061788bd20337dee1ec24db6c0c0e87a6
Scott Kloustin <skloustin@gmail.com>
2022-04-11 20:24:31 -0400

    basic search working

--- vue/src/components/EventSearch.vue
+++ vue/src/components/EventSearch.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-      <h2>This is the title of the Event Search</h2>
-      <div v-for="event in events" v-bind:key="event.id">
+      <h2>Title of the Event Search</h2>
+      <input type="text" placeholder="Type an event" v-model="eventSearchString" />
+      <div v-for="event in filteredEvents" v-bind:key="event.id">
          <hr>
           <h2>{{ event.eventName }}</h2>
           <p>{{ event.information }}</p>
+
       </div>
   </div>
 </template>
@@ -17,14 +19,38 @@ export default {
     name: 'EventSearch',
     data(){
         return {
-            events:[]
+          
+            eventsAll:[],
+            eventSearchString: "",
         }
     },
+    computed: {
+        filteredEvents() {
+            let events = this.eventsAll;
+            let searchString = this.eventSearchString;
+            let cleanSearchString = searchString.trim().toLowerCase();
+
+             // if empty search string
+            if(!searchString){
+                return events;
+            }
+
+            events = events.filter((event) => {
+                if(event.eventName.toLowerCase().indexOf(cleanSearchString) !== -1){
+                    return event
+                }
+            })
+
+            return events;
+        }
+
+    },
+
 
     created(){
         eventService.getEvents()
         .then((response) => {
-            this.events = response.data;
+            this.eventsAll = response.data;
             console.log(response.data);
         })
     }
