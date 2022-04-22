4f3f4e24fc643dbffe977e171af20a6070efdda0
Scott Kloustin <skloustin@gmail.com>
2022-04-21 20:58:06 -0400

    updated about page images

--- vue/src/views/About.vue
+++ vue/src/views/About.vue
@@ -6,27 +6,31 @@
       <b-row>
         <b-col>
           <div class="deserae">
-            <img class="dolphin" src="..\images\JamDolph.png" />
+            <img class="mx-auto d-block dolphin" src="..\images\JamDolph.png" />
             <h2 class = "name"> Deserae </h2>
+            <h3 class = "about">Deserae is a local legend who belonged to multiple sororities while in college and none of them knew about the others.</h3>
           </div>
         </b-col>
         <b-col><div class="lily">
           <center>
-          <img class="cow"  src="..\images\LilyJamCow.png" />
+          <img class="mx-auto d-block cow"  src="..\images\LilyJamCow.png" />
           </center>
         <h2 class = "name"> Lily </h2>
+        <h3 class = "about">Lily once threw a rager in a Dick's Sporting Goods that was written up in a local paper.</h3>
         </div></b-col>
       </b-row>
        <b-row>
         <b-col>
           <div class="lindsey">
-            <img src="..\images\dogmusic.gif" />
+            <img class="mx-auto d-block seahorse" src="..\images\SeahorseJamLindsey.png" />
             <h2 class = "name"> Lindsey </h2>
+            <h3 class = "about">Lindsey asked Nic Cage to show up to her birthday one year... and he actually did.</h3>
             </div>
         </b-col>
         <b-col><div class="scott">
-          <img class="beta" src="..\images\BetaPurpBlueLogo.png" />
+          <img class="mx-auto d-block beta" src="..\images\BetaPurpBlueLogo.png" />
         <h2 class = "name"> Scott </h2>
+        <h3 class = "about">Scott threw a party in three states at the same time and when the cops showed up, they joined the party... it was that good.</h3>
         </div></b-col>
       </b-row>
 </b-container>
@@ -51,6 +55,7 @@ export default {
   font-family: "Neonderthaw";
   text-align: center;
   color: 0278E4;
+  font-size: 100px;
 }
 .deserae{
     border:#003B77 5px solid;
@@ -79,16 +84,21 @@ export default {
    
 }
 .dolphin{
-  height: 50vh;
-  width: 65vh;
+  max-height: 350px;
+  
 }
 .beta{
-  height: 50vh;
-  width: 65vh;
+  max-height: 350px;
 }
 .cow{
-   height: 40vh;
-   width: 30vh;
+   max-height: 350px;
    
+}
+.seahorse {
+  max-height: 350px;
+}
+
+.about{
+  font-family: "Audiowide";
 }
 </style>
